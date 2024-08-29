0785c2739c02cb3c94f2837a1df0e63a7de462ed
Sayam Samal <sayam@zulip.com>
2024-08-29 05:32:45 +0530

    banners: Add desktop notification banner.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let step = 0;
+
+const desktop_notification_banner = document.querySelector("[data-process='notifications']");
+const banner_close_btn = desktop_notification_banner.querySelector(".banner__close-btn");
+
+banner_close_btn.addEventListener("click", () => {
+    const content = desktop_notification_banner.querySelectorAll(".banner__content");
+    content[step].classList.add("hidden");
+    console.log(step);
+    step = (step+1) % content.length;
+    console.log(step);
+    content[step].classList.remove("hidden");
+});
